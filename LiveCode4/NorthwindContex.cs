af721e269e4429839971133bf685376e3ffd2aeb
Søren Johansen <soerenzig@gmail.com>
2017-10-17 13:49:12 +0200

    init commit

---
+++ LiveCode4/NorthwindContex.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveCode4
+{
+    class NorthwindContex : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseMySql(
+                "server=localhost;database=northwind;uid=root;pwd=Zignation123");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>()
+                .Property(x => x.Name).HasColumnName("categoryname");
+
+        }
+    }
+}
---
+++ LiveCode4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace LiveCode4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new NorthwindContex())
+            {
+                
+                var categories = db.Categories
+                    .Where(x => x.Id < 8);
+                
+
+                foreach (var category in categories)
+                {
+                    Console.WriteLine(category.Id + "|\t|" + category.Name + "\t|" + category.Description);
+                }
+
+                
+
+                //var category = db.Categories.FirstOrDefault(
+                //    x => x.Id == 11);
+
+                // update
+                //if (category != null)
+                //{
+                //    category.Name = "2017 Testing";
+                //}
+
+                // delete
+                //db.Categories.Remove(category);
+
+                 var addCategory = new Category
+                {
+                    Name = "Cake",
+                    Description = "cupcake"
+                };
+
+                db.Categories.Add(addCategory);
+                db.SaveChanges(); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
